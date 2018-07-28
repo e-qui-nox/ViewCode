66fc00582600655468fc0cd4681dcc3e14493c52
e-qui-nox <yamuna.pixie@gmail.com>
2018-07-28 18:34:28 +0530

    First Commit

---
+++ Codeforces/Problemset/A/Minimum Binary Number.cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,i;
+    string s;
+    cin>>n;
+    cin>>s;
+    if (s=="0") {
+        cout<<'0';
+        return 0;
+    }
+    cout<<'1';
+    for(i=0; i<n; i++){
+        if (s[i]=='0') 
+            cout<<'0';
+    }
+}
---
+++ Codeforces/Problemset/A/Paper Airplanes.cpp
@@ -0,0 +1,10 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int k,n,s,p;
+	cin>>k>>n>>s>>p;
+	int ans=k*((n+s-1)/s);
+	cout<<(ans+p-1)/p;
+    return 0;
+}
