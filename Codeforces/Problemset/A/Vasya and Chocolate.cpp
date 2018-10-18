22fbf05ec20fd412e6948237942d10204fc31805
e-qui-nox <yamuna.pixie@gmail.com>
2018-10-18 23:55:28 +0530

    First Commit

---
+++ Codeforces/Problemset/A/Vasya and Chocolate.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long s,a,b,c,x;
+	    cin>>s>>a>>b>>c;
+	    x = s/c;
+	    cout<<x+(x/a)*b<<endl;
+	}
+}
---
+++ Codeforces/Problemset/A/Vova and Train.cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   int t;
+    cin>>t;
+    while(t--)
+    {
+	    int x,v,l,r,a,b,c;
+	    cin>>x>>v>>l>>r;
+	    a = (x/v);
+	    b = (r/v);
+	    c = (l-1);
+	    cout<<a-b+c/v<<endl;
+    }
+}
